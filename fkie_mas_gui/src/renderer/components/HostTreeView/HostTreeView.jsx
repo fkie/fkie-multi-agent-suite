bba1c414ce3adb875f240164123dbd9c5364aabe
Alexander Tiderko <Alexander.Tiderko@fkie.fraunhofer.de>
2024-02-20 10:35:16 +0100

    fixed visualized time difference

--- fkie_mas_gui/src/renderer/components/HostTreeView/HostTreeView.jsx
+++ fkie_mas_gui/src/renderer/components/HostTreeView/HostTreeView.jsx
@@ -597,27 +597,7 @@ function HostTreeView({
 
   function formatTime(milliseconds) {
     const sec = (milliseconds / 1000.0).toFixed(3);
-    let hours = Math.floor(sec / 3600);
-    let minutes = Math.floor((sec - hours * 3600) / 60);
-    let seconds = sec - hours * 3600 - minutes * 60;
-
-    if (hours < 10) {
-      hours = '0' + hours;
-    }
-    if (minutes < 10) {
-      minutes = '0' + minutes;
-    }
-    if (seconds < 10) {
-      seconds = '0' + seconds;
-    }
-    let result = "";
-    if (hours > 0) {
-      result += hours + 'h ';
-    }
-    if (minutes > 0) {
-      result += minutes + 'm ';
-    }
-    return result + seconds + 's';
+    return sec + 's';
   }
 
   /**
