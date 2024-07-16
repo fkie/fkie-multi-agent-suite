72d3456e2e6f8e3edfa5517114fd0bee30f44bb9
Alexander Tiderko <Alexander.Tiderko@fkie.fraunhofer.de>
2024-07-16 14:14:25 +0200

    fixed host line height

--- fkie_mas_gui/src/renderer/components/HostTreeView/HostTreeViewItem.jsx
+++ fkie_mas_gui/src/renderer/components/HostTreeView/HostTreeViewItem.jsx
@@ -183,7 +183,9 @@ function HostTreeViewItem({
                   onButtonIconClick();
                 }}
               >
-                <ButtonIcon sx={{ color: buttonIconColor }} />
+                <ButtonIcon
+                  sx={{ color: buttonIconColor, fontSize: "inherit" }}
+                />
               </IconButton>
             </Tooltip>
           )}
