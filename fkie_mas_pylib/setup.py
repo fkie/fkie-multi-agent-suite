7b4d9c064c0dd1929c9b379f4f03b31a78dbc81f
Alexander Tiderko <Alexander.Tiderko@fkie.fraunhofer.de>
2025-01-13 14:29:42 +0100

    fixed setup.py of pylib

--- fkie_mas_pylib/setup.py
+++ fkie_mas_pylib/setup.py
@@ -14,6 +14,7 @@ if 'ROS_VERSION' in os.environ and os.environ['ROS_VERSION'] == '1':
                   package_name + '.launch',
                   package_name + '.logging',
                   package_name + '.parameters',
+                  package_name + '.service',
                   package_name + '.system',
                   package_name + '.websocket'],
         package_dir={'': '.'}
@@ -47,6 +48,7 @@ else:
                   package_name + '.launch',
                   package_name + '.logging',
                   package_name + '.parameters',
+                  package_name + '.service',
                   package_name + '.system',
                   package_name + '.websocket'],
         data_files=[
