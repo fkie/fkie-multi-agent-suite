8821914956845b4d9334d0f9dc496141482e85bd
Silencio <77356433+S1lencio@users.noreply.github.com>
2025-03-20 10:38:37 +0100

    support fstring, add keyword highlighting

--- fkie_mas_gui/src/renderer/components/MonacoEditor/PythonLaunchHighlighter.ts
+++ fkie_mas_gui/src/renderer/components/MonacoEditor/PythonLaunchHighlighter.ts
@@ -3,6 +3,145 @@ import { languages } from "monaco-editor/esm/vs/editor/editor.api";
 export const PythonLanguage: languages.IMonarchLanguage = {
     defaultToken: '',
 
+	keywords: [
+		// This section is the result of running
+		// `for k in keyword.kwlist: print('  "' + k + '",')` in a Python REPL,
+		// though note that the output from Python 3 is not a strict superset of the
+		// output from Python 2.
+		'False', // promoted to keyword.kwlist in Python 3
+		'None', // promoted to keyword.kwlist in Python 3
+		'True', // promoted to keyword.kwlist in Python 3
+		'and',
+		'as',
+		'assert',
+		'async', // new in Python 3
+		'await', // new in Python 3
+		'break',
+		'class',
+		'continue',
+		'def',
+		'del',
+		'elif',
+		'else',
+		'except',
+		'exec', // Python 2, but not 3.
+		'finally',
+		'for',
+		'from',
+		'global',
+		'if',
+		'import',
+		'in',
+		'is',
+		'lambda',
+		'nonlocal', // new in Python 3
+		'not',
+		'or',
+		'pass',
+		'print', // Python 2, but not 3.
+		'raise',
+		'return',
+		'try',
+		'while',
+		'with',
+		'yield',
+
+		'int',
+		'float',
+		'long',
+		'complex',
+		'hex',
+
+		'abs',
+		'all',
+		'any',
+		'apply',
+		'basestring',
+		'bin',
+		'bool',
+		'buffer',
+		'bytearray',
+		'callable',
+		'chr',
+		'classmethod',
+		'cmp',
+		'coerce',
+		'compile',
+		'complex',
+		'delattr',
+		'dict',
+		'dir',
+		'divmod',
+		'enumerate',
+		'eval',
+		'execfile',
+		'file',
+		'filter',
+		'format',
+		'frozenset',
+		'getattr',
+		'globals',
+		'hasattr',
+		'hash',
+		'help',
+		'id',
+		'input',
+		'intern',
+		'isinstance',
+		'issubclass',
+		'iter',
+		'len',
+		'locals',
+		'list',
+		'map',
+		'max',
+		'memoryview',
+		'min',
+		'next',
+		'object',
+		'oct',
+		'open',
+		'ord',
+		'pow',
+		'print',
+		'property',
+		'reversed',
+		'range',
+		'raw_input',
+		'reduce',
+		'reload',
+		'repr',
+		'reversed',
+		'round',
+		'self',
+		'set',
+		'setattr',
+		'slice',
+		'sorted',
+		'staticmethod',
+		'str',
+		'sum',
+		'super',
+		'tuple',
+		'type',
+		'unichr',
+		'unicode',
+		'vars',
+		'xrange',
+		'zip',
+
+		'__dict__',
+		'__methods__',
+		'__members__',
+		'__class__',
+		'__bases__',
+		'__name__',
+		'__mro__',
+		'__subclasses__',
+		'__init__',
+		'__import__'
+	],
+
 	brackets: [
 		{ open: '{', close: '}', token: 'delimiter.curly' },
 		{ open: '[', close: ']', token: 'delimiter.bracket' },
@@ -18,16 +157,10 @@ export const PythonLanguage: languages.IMonarchLanguage = {
 			[/[,:;]/, 'delimiter'],
 			[/[{}\[\]()]/, '@brackets'],
 
-            //imports
-            // TODO: this should match both "from a import b" and "import a", but only matches the former.
-            [/(from)?(.+)?(import)(.+)/, 
-                [
-                    "tag",
-                    "attribute.name",
-                    "tag",
-                    "attribute.name",
-                ]
-            ],
+			[/@[a-zA-Z_]\w*/, 'tag'],
+			[/[a-zA-Z_]\w*/, {
+				cases: { '@keywords': 'tag', }
+			}],
 		],
 
 		// Deal with white space, including single and multi-line comments
@@ -58,9 +191,19 @@ export const PythonLanguage: languages.IMonarchLanguage = {
 		// Recognize strings, including those broken across lines with \ (but not without)
 		strings: [
 			[/'$/, 'string.escape', '@popall'],
+			[/f'{1,3}/, 'string.escape', '@fStringBody'],
 			[/'/, 'string.escape', '@stringBody'],
 			[/"$/, 'string.escape', '@popall'],
+			[/f"{1,3}/, 'string.escape', '@fDblStringBody'],
 			[/"/, 'string.escape', '@dblStringBody']
+		],
+		fStringBody: [
+			[/[^\\'\{\}]+$/, 'string', '@popall'],
+			[/[^\\'\{\}]+/, 'string'],
+			[/\{[^\}':!=]+/, 'identifier', '@fStringDetail'],
+			[/\\./, 'string'],
+			[/'/, 'string.escape', '@popall'],
+			[/\\$/, 'string']
 		],
 		stringBody: [
 			[/[^\\']+$/, 'string', '@popall'],
@@ -69,12 +212,26 @@ export const PythonLanguage: languages.IMonarchLanguage = {
 			[/'/, 'string.escape', '@popall'],
 			[/\\$/, 'string']
 		],
+		fDblStringBody: [
+			[/[^\\"\{\}]+$/, 'string', '@popall'],
+			[/[^\\"\{\}]+/, 'string'],
+			[/\{[^\}':!=]+/, 'identifier', '@fStringDetail'],
+			[/\\./, 'string'],
+			[/"/, 'string.escape', '@popall'],
+			[/\\$/, 'string']
+		],
 		dblStringBody: [
 			[/[^\\"]+$/, 'string', '@popall'],
 			[/[^\\"]+/, 'string'],
 			[/\\./, 'string'],
 			[/"/, 'string.escape', '@popall'],
 			[/\\$/, 'string']
+		],
+		fStringDetail: [
+			[/[:][^}]+/, 'string'],
+			[/[!][ars]/, 'string'],  // only !a, !r, !s are supported by f-strings: https://docs.python.org/3/tutorial/inputoutput.html#formatted-string-literals
+			[/=/, 'string'],
+			[/\}/, 'identifier', '@pop']
 		]
 	}
 };
